127fb4fabe96b6e30167267a97ec5fc9ea588e40
Durgesh Kumar <durgeshkumar7b@gmail.com>
2023-09-17 23:40:12 +0530

    Create Many_row_to_one_row.sql

---
+++ Multiple Row in One row/Many_row_to_one_row.sql
@@ -0,0 +1,18 @@
+/*cREATE TABLE*/
+
+-- CREATE TABLE CAN_CAMPAIGN(
+-- CANDIDATE VARCHAR(12),
+-- CAMPAIGN VARCHAR(12)
+-- )
+
+
+-- INSERT INTO can_campaign VALUES
+-- ('Carolyn','MNC'),
+-- ('Carolyn','YUI'),
+-- ('Melessa','MNC'),
+-- ('Melessa','YUI'),
+-- ('Melessa','MNC')
+
+
+/*Write a Query to show all campaign in one row for each candidtes */
+
